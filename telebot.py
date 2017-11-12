77a0568cfca49be20049f90e8ec4af6ef7aee89e
Jefferson Oliveira <JeffOlivr@users.noreply.github.com>
2017-11-12 17:32:50 -0200

    Add update_id parameter to get_updates

--- telebot.py
+++ telebot.py
@@ -11,30 +11,51 @@ class Bot:
     def __init__(self, access_token):
         """Set up the access_token and defines the request URL.
 
-        @param (str): Telegram access token.
+        Args:
+            @param (str): Telegram access token.
         """
         self.access_token = access_token
         self.url = "https://api.telegram.org/bot" + access_token + "/"
 
     def get_me(self):
-        """Return information about the bot."""
+        """Return information about the bot.
+
+        Returns:
+            Dict with the bot information.
+
+        """
         method = self.url + 'getme'
         return json.loads(requests.post(method).content)
 
     def send_message(self, chat_id, text):
         """Send a message to the chat.
 
-        @param chat_id (int): The chat_id (which is also the user id)
-        @param text (str): Message
+        Args:
+            @param chat_id (int): The chat_id (which is also the user id)
+            @param text (str): Message
+        Returns:
+            Dict with the request response.
+
         """
         method = self.url + 'sendMessage'
         params = {"chat_id": chat_id, "text": text}
         return json.loads(requests.post(method, data=params).content)
 
-    def get_updates(self):
-        """Get messages updates."""
+    def get_updates(self, update_id=0):
+        """Get messages updates.
+
+        If update_id is set, return messages after the update+Id.
+
+        Args:
+            @param update_id (int): Telegram update_id
+
+        Returns:
+            Dict with the messages.
+
+        """
         method = self.url + 'getUpdates'
-        return json.loads(requests.get(method).content)
+        params = update_id and ("?offset=" + update_id) or ""
+        return json.loads(requests.get(method + params).content)
 
 
 if __name__ == "__main__":
